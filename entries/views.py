aaf2dc097e3baca61fbd2114bd96df6ebe5337d9
Тарас <frost.44@ya.ru>
2025-01-16 14:26:31 +0300

    commit 2

---
+++ entries/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+from .models import Entry
+
+
+# Create your views here.
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by('-date_created')
+
+
+class EntryDetailView(DetailView):
+    model = Entry
+
+
